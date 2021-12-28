54add0118dbc86563a79c3927e69d3e18d89f124
Leo Liu <auleo.liu@gmail.com>
2021-12-28 11:51:03 +1100

    alpha

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pandas_df_sql',
-      version='0.1',
+      version='0.2',
       description='Perform SQL queries on pandas dataframes',
       author='Leo Liu',
       author_email='leo.liu@unsw.edu.au',
